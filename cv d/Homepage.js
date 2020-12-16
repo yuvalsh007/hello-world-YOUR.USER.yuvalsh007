9619d25a9f78c4f9709ea32d709fb3998c8bde8d
yuvalsh007 <73237697+yuvalsh007@users.noreply.github.com>
2020-12-16 21:06:48 +0200

    Add files via upload
    project 6

---
+++ cv d/Homepage.js
@@ -0,0 +1,11 @@
+function scrollToCV(){
+  window.scrollBy(0, 610);
+}
+
+function scrollToContact(){
+  window.scrollBy(0, 1220);
+}
+
+function sendEmail(){
+  document.getElementById('emailIcon').style.filter= "grayscale(0%)";
+}
