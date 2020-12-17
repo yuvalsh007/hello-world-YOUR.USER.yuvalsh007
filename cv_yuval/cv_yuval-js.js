8f00259574a3ec152f4356098688a4f6c478ad74
yuvalsh007 <73237697+yuvalsh007@users.noreply.github.com>
2020-12-17 22:53:23 +0200

    Add files via upload
    cv_yuval_6

---
+++ cv_yuval/cv_yuval-js.js
@@ -0,0 +1,12 @@
+function scrollToCV(){
+    window.scrollBy(0, 610);
+  }
+  
+  function scrollToContact(){
+    window.scrollBy(0, 1220);
+  }
+  
+  function sendEmail(){
+    document.getElementById('emailIcon').style.filter= "grayscale(0%)";
+  }
+  
